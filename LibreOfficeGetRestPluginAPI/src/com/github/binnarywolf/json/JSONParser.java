3f4dba03cf195574a9251a18c60bce78096f681c
Dmitriy Bazunov <BinnaryWolf@gmail.com>
2014-05-31 15:42:06 +0000

    Add basic json parse + test

--- LibreOfficeGetRestPluginAPI/src/com/github/binnarywolf/json/JSONParser.java
+++ LibreOfficeGetRestPluginAPI/src/com/github/binnarywolf/json/JSONParser.java
@@ -1,5 +1,57 @@
 package com.github.binnarywolf.json;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 public class JSONParser {
 
+	public static String parseJSON(String json, String parsePattern) {
+		String result = json;
+		try {
+			String[] commands = parsePattern.trim().split("\\.");
+			System.out.println(Arrays.toString(commands));
+			ObjectMapper mapper = new ObjectMapper();
+			JsonFactory factory = mapper.getJsonFactory();
+			JsonParser jp;
+			jp = factory.createJsonParser(json);
+
+			JsonNode actualObj = mapper.readTree(jp);
+			for (String command : commands) {
+				actualObj = executeCommand(actualObj, command);
+			}
+			if (actualObj.isTextual()) {
+				result = actualObj.asText();
+			} else {
+				result=actualObj.toString();
+			}
+		} catch (JsonParseException e) {
+			result = "JSON parser exception";
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private static JsonNode executeCommand(JsonNode currentNode, String command) {
+		JsonNode newNode = null;
+		System.out.println("Node before " + currentNode);
+		String argument = command.substring(4, command.length() - 1);
+		try {
+			int index = Integer.parseInt(argument);
+			newNode = currentNode.get(index);
+		} catch (NumberFormatException e) {
+			newNode = currentNode.get(argument);
+		}
+		System.out.println("Node after " + newNode);
+		return newNode;
+	}
 }
--- LibreOfficeGetRestPluginAPI/src/com/github/binnarywolf/test/RestTest.java
+++ LibreOfficeGetRestPluginAPI/src/com/github/binnarywolf/test/RestTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.Request;
 
+import com.github.binnarywolf.json.JSONParser;
 import com.github.binnarywolf.rest.RestClient;
 
 public class RestTest {
@@ -16,5 +17,21 @@ public class RestTest {
 		String expectedResponse = "[{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Europe\",\"estimatedPercent\":19.656862459382253,\"estimatedAmount\":3.0764562297929977E10,\"confidence\":0.9002921987357246},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Americas\",\"estimatedPercent\":46.43808654415262,\"estimatedAmount\":7.267932048852238E10,\"confidence\":0.977866925720263},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"China\",\"estimatedPercent\":14.04230209882504,\"estimatedAmount\":2.1977326168829094E10,\"confidence\":0.98681127},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Asia\",\"estimatedPercent\":24.789481072155393,\"estimatedAmount\":3.8797521036408966E10,\"confidence\":0.9718888175064978}]";
 		String response = client.get(url);
 		assertEquals(expectedResponse, response);
+	}
+
+	@Test
+	public void parseJSONTest() {
+		String json = "[{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Europe\",\"estimatedPercent\":19.656862459382253,\"estimatedAmount\":3.0764562297929977E10,\"confidence\":0.9002921987357246},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Americas\",\"estimatedPercent\":46.43808654415262,\"estimatedAmount\":7.267932048852238E10,\"confidence\":0.977866925720263},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"China\",\"estimatedPercent\":14.04230209882504,\"estimatedAmount\":2.1977326168829094E10,\"confidence\":0.98681127},{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Asia\",\"estimatedPercent\":24.789481072155393,\"estimatedAmount\":3.8797521036408966E10,\"confidence\":0.9718888175064978}]";
+		String[] commands = { "get(0)", "get(0).get(requestedIdentifierType)",
+				"get(0).get(region)", "get(1).get(region)",
+				"get(2).get(region)", "get(3).get(region)" };
+		String[] parseResults = new String[commands.length];
+		String[] expected = {
+				"{\"requestedIdentifier\":\"777\",\"requestedIdentifierType\":\"REVID\",\"requestedDate\":\"2014-05-30\",\"companyName\":\"Apple Inc\",\"companyTicker\":\"AAPL\",\"companyRevereId\":\"777\",\"periodStart\":\"2011-09-30\",\"periodEnd\":\"2012-09-29\",\"region\":\"Europe\",\"estimatedPercent\":19.656862459382253,\"estimatedAmount\":3.0764562297929977E10,\"confidence\":0.9002921987357246}",
+				"REVID", "Europe", "Americas","China","Asia"};
+		for (int i = 0; i < commands.length; i++) {
+			parseResults[i]=JSONParser.parseJSON(json,commands[i]);
+		}
+		assertArrayEquals(expected, parseResults);
 	}
 }
