8e7baa9147db4a31518174164d0c293c2a0222b5
binnarywolf <BinnaryWolf@gmail.com>
2014-10-08 23:59:15 +0400

    fix project dependencies + some test logs

--- LibreOfficeRestAPI/src/main/java/com/github/binnarywolf/json/JSONParser.java
+++ LibreOfficeRestAPI/src/main/java/com/github/binnarywolf/json/JSONParser.java
@@ -1,4 +1,4 @@
-package com.github.binnarywolf.json; 
+package com.github.binnarywolf.json;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -10,54 +10,72 @@ import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-public class JSONParser {
+public class JSONParser
+{
 
-	public static String parseJSON(String json, String parsePattern) {
-		String result = json;
-		try {
-			String[] commands = parsePattern.trim().split("\\.");
-			System.out.println(Arrays.toString(commands));
-			ObjectMapper mapper = new ObjectMapper();
-			JsonFactory factory = mapper.getJsonFactory();
-			JsonParser jp;
-			jp = factory.createJsonParser(json);
+    public static String parseJSON(String json, String parsePattern)
+    {
+        System.out.println("Start json");
+        String result = json;
+        try {
+            String[] commands = parsePattern.trim().split("\\.");
+            System.out.println(Arrays.toString(commands));
 
-			JsonNode actualObj = mapper.readTree(jp);
-			for (String command : commands) {
-				actualObj = executeCommand(actualObj, command);
-			}
-			if (actualObj != null) {
-				if (actualObj.isTextual()) {
-					result = actualObj.asText();
-				} else {
-					result = actualObj.toString();
-				}
-			} else {
-				result = "No such node found.";
-			}
-		} catch (JsonParseException e) {
-			result = "JSON parser exception";
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
-	}
+            System.out.println("before map");
+            ObjectMapper mapper = new ObjectMapper();
 
-	private static JsonNode executeCommand(JsonNode currentNode, String command) {
-		JsonNode newNode = null;
-		if (currentNode != null) {
-			System.out.println("Node before " + currentNode);
-			String argument = command.substring(4, command.length() - 1);
-			if (command.startsWith("get(")) {
-				int index = Integer.parseInt(argument);
-				newNode = currentNode.get(index);
-			} else {
-				newNode = currentNode.get(command);
-			}
-			System.out.println("Node after " + newNode);
-		}
-		return newNode;
-	}
+            System.out.println("after map");
+            JsonFactory factory = mapper.getJsonFactory();
+            JsonParser jp;
+            jp = factory.createJsonParser(json);
+
+            JsonNode actualObj = mapper.readTree(jp);
+            for (String command : commands) {
+                actualObj = executeCommand(actualObj, command);
+            }
+            if (actualObj != null) {
+                if (actualObj.isTextual()) {
+                    result = actualObj.asText();
+                }
+                else {
+                    result = actualObj.toString();
+                }
+            }
+            else {
+                result = "No such node found.";
+            }
+        }
+        catch (JsonParseException e) {
+            result = "JSON parser exception";
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            System.out.println("Unknown exception: " + e);
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static JsonNode
+            executeCommand(JsonNode currentNode, String command)
+    {
+        JsonNode newNode = null;
+        if (currentNode != null) {
+            System.out.println("Node before " + currentNode);
+            String argument = command.substring(4, command.length() - 1);
+            if (command.startsWith("get(")) {
+                int index = Integer.parseInt(argument);
+                newNode = currentNode.get(index);
+            }
+            else {
+                newNode = currentNode.get(command);
+            }
+            System.out.println("Node after " + newNode);
+        }
+        return newNode;
+    }
 }
---
+++ LibreOfficeRestAPI/src/test/java/com/github/binnarywolf/test/RestTest.java
@@ -0,0 +1,8 @@
+
+package com.github.binnarywolf.test;
+
+public class RestTest {
+
+
+
+}
